294ee4534c4595b8e7080437767343eedb2059cf
bKlamann92 <bartosz.klamann@gmail.com>
2022-04-03 13:49:27 +0200

    Initial

---
+++ src/main/java/pl/sda/arppl2/maven/Main.java
@@ -0,0 +1,10 @@
+package pl.sda.arppl2.maven;
+
+public class Main {
+    // psvm - generuje metodę startową - każdy projekt musi ją mieć!
+    public static void main(String[] args) {
+        //sout - generuje komendę wpisywania do konsoli
+        // wypisz linię
+        System.out.println("Hello World!");
+    }
+}
